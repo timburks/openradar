f51c49840a60371979ebbeb224f734ceba1650b9
Tim Burks <tim@neontology.com>
2008-11-15 21:58:54 -0800

    New model properties, additional info links.
    I added some new model properties to cover more of the specific
    items in the bugreport data store, restored user names to the
    main index (now that emails addresses are cut out), and included
    a link to our twitter id in the page footer.

--- models.py
+++ models.py
@@ -1,13 +1,17 @@
 from google.appengine.ext import db
 
 class Radar(db.Model):
-  number = db.StringProperty()
-  title = db.StringProperty()
-  status = db.StringProperty()
-  user = db.UserProperty()
-  description = db.TextProperty()
-  created = db.DateTimeProperty()
-  modified = db.DateTimeProperty()
+  number = db.StringProperty()		# the Radar Problem ID (we need an int form of this)
+  title = db.StringProperty()		 
+  status = db.StringProperty()		# Radar state
+  user = db.UserProperty()		# App Engine user who created this entry
+  product = db.StringProperty()		
+  classification = db.StringProperty()
+  description = db.TextProperty()	# problem description plus anything else.
+  originated = db.DateTimeProperty()	# when the Radar was filed
+  created = db.DateTimeProperty()	# when the OpenRadar object was created
+  modified = db.DateTimeProperty()	# when the OpenRadar object was last modified
+
   def username(self):
     return self.user.nickname().split("@")[0]
 
