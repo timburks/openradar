76798105c98ff4337613dea373621a75f8ef178e
cverwymeren <verwymeren@gmail.com>
2010-09-16 21:27:59 -0500

    Added method for retrieving radars submitted by a specific user.

--- openradar/db.py
+++ openradar/db.py
@@ -9,4 +9,8 @@ class Radar():
         
     def fetchByNumber(self, number):
         return models.Radar.gql("WHERE number = :1", number).get()
+        
+    def fetchByUser(self, user, page = 1, count = 100):
+        return models.Radar.gql("WHERE user = :1 ORDER BY number DESC").fetch(count, (page - 1) * count)
+        
 
