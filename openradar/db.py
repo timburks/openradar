42934c403fc87b043d2344158a4b1af68cc4b722
cverwymeren <verwymeren@gmail.com>
2010-09-16 21:35:54 -0500

    Added class for retrieving comments from the database.

--- openradar/db.py
+++ openradar/db.py
@@ -13,4 +13,8 @@ class Radar():
     def fetchByUser(self, user, page = 1, count = 100):
         return models.Radar.gql("WHERE user = :1 ORDER BY number DESC").fetch(count, (page - 1) * count)
         
+class Comment():
+    def fetchAll(self, page = 1, count = 100):
+        return models.Comment.gql("ORDER BY posted_at DESC").fetch(count, (page - 1) * count)
+    
 
