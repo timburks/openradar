2d1fd7968aba213d23e8d25527c1f0f43b94df9f
cverwymeren <verwymeren@gmail.com>
2010-09-16 22:28:19 -0500

    Added missing 'result' key to the returned JSON object.

--- openradar/api.py
+++ openradar/api.py
@@ -5,25 +5,27 @@ import models
 
 class Radar(handlers.Handler):
     def get(self):
-        radar = None
+        result = {}
         radarId = self.request.get("id")
         if radarId:
-            radar = db.Radar().getById(radarId)
+            radar = db.Radar().fetchById(radarId)
             if (radar):
-                self.respondWithDictionaryAsJSON(radar.toDictionary())
-            return
-        radarNumber = self.request.get("number")
-        if radarNumber:
-            radar = db.Radar().getByNumber(radarNumber)
-            if (radar):
-                self.respondWithDictionaryAsJSON(radar.toDictionary())
-            return
-    
+                result = radar.toDictionary()
+        if not result:
+            radarNumber = self.request.get("number")
+            if radarNumber:
+                radar = db.Radar().fetchByNumber(radarNumber)
+                if (radar):
+                    result = radar.toDictionary()
+        self.respondWithDictionaryAsJSON({"result": result})
+        
     def post(self):
         pass
+        
 
 class Test(handlers.Handler):
     def get(self):
         result = {"foo":[1, 2, 3, {"bar": [4, 5, 6]}]}
         self.respondWithDictionaryAsJSON(result)
+        
 
