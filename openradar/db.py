c8c311d33354b9468355913b87ab4e0dffb5b42c
cverwymeren <verwymeren@gmail.com>
2010-09-22 15:53:54 -0500

    Added method for retrieving all radars matching a specified set of radar numbers.

--- openradar/db.py
+++ openradar/db.py
@@ -10,6 +10,9 @@ class Radar():
     def fetchByNumber(self, number):
         return models.Radar.gql("WHERE number = :1", number).get()
         
+    def fetchByNumbers(self, numbers, page = 1, count = 100):
+        return models.Radar.gql("WHERE number IN :1", numbers).fetch(count, (page - 1) * count)
+        
     def fetchByUser(self, user, page = 1, count = 100):
         return models.Radar.gql("WHERE user = :1 ORDER BY number DESC", user).fetch(count, (page - 1) * count)
         
