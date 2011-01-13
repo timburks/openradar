6ea842b741bdf59f98e6ed1cd3a2eb6332454393
cverwymeren <verwymeren@gmail.com>
2011-01-12 21:09:40 -0600

    Radar.sanitize: Added conditionals to verify a variable contains data before attempting to access it.

--- openradar/models.py
+++ openradar/models.py
@@ -1,101 +1,119 @@
 from google.appengine.ext import db
 from google.appengine.ext import search
 import datetime
+import markdown
 
 class Secret(db.Model):
-  name = db.StringProperty()
-  value = db.StringProperty()
+    name = db.StringProperty()
+    value = db.StringProperty()
 
 class Radar(search.SearchableModel):
-  # This first set of properties are user-specified. 
-  # For flexibility and robustness, we represent them all as strings.
-  # The Radar Problem ID (we need an int form of this)
-  number = db.StringProperty()
-  # The radar number this radar duplicates
-  parent_number = db.StringProperty()
-  title = db.StringProperty()
-  status = db.StringProperty()		# Radar state
-  resolved = db.StringProperty()
-  user = db.UserProperty()		# App Engine user who created this entry
-  product = db.StringProperty()		
-  classification = db.StringProperty()
-  reproducible = db.StringProperty()
-  product_version = db.StringProperty()
-  description = db.TextProperty()	# problem description plus anything else.
-  originated = db.StringProperty()	# when the Radar was filed
+    # This first set of properties are user-specified.
+    # For flexibility and robustness, we represent them all as strings.
+    # The Radar Problem ID (we need an int form of this)
+    number = db.StringProperty()
+    
+    # The radar number this radar duplicates
+    parent_number = db.StringProperty()
+    title = db.StringProperty()
+    # Radar state
+    status = db.StringProperty()
+    resolved = db.StringProperty()
+    # App Engine user who created this entry
+    user = db.UserProperty()
+    product = db.StringProperty()
+    classification = db.StringProperty()
+    reproducible = db.StringProperty()
+    product_version = db.StringProperty()
+    # problem description plus anything else.
+    description = db.TextProperty()
+    # when the Radar was filed
+    originated = db.StringProperty()
+    # when the OpenRadar object was created
+    created = db.DateTimeProperty()
+    # These remaining properties are managed by the OpenRadar web app.
+    # They are automatically set when put() is called.
+    # We will add more as needed to allow better performance or to simplify
+    # sorting and querying.
+    # when the OpenRadar object was last modified
+    modified = db.DateTimeProperty()
+    
+    def username(self):
+        return self.user.nickname().split("@")[0]
+    
+    def put(self):
+        self.modified = datetime.datetime.now()
+        # Sanitize the data before storing
+        self.sanitize()
+        return db.Model.put(self)
+    
+    def comments(self):
+        return Comment.gql("WHERE radar = :1 AND is_reply_to = :2", self, None)
+    
+    def children(self):
+        return Radar.gql("WHERE parent_number = :1 ORDER BY number ASC", self.number)
+    
+    def parent(self):
+        return Radar.gql("WHERE number = :1", self.parent_number).get()
+    
+    def sanitize(self):
+        if (self.classification):
+            self.classification = self.classification.strip()
+        if (self.description):
+            self.description = self.description.strip()
+        if (self.number):
+            self.number = self.number.strip()
+        if (self.originated):
+            self.originated = self.originated.strip()
+        if (self.product):
+            self.product = self.product.strip()
+        if (self.product_version):
+            self.product_version = self.product_version.strip()
+        if (self.resolved):
+            self.resolved = self.resolved.strip()
+        if (self.reproducible):
+            self.reproducible = self.reproducible.strip()
+        
+        # The most common format for duplicates is "Duplicate/<radar_number>"
+        # If that format is found, extract the included radar number and store
+        # it in self.parent_number
+        if (self.status):
+            current_status = self.status.strip()
+            status_words = current_status.split("/")
+            if (len(status_words) == 2):
+                # Trim any leading or trailing whitespace from the status type
+                status_type = status_words[0].strip()
+                # Determine whether status_type equals "duplicate", ignore
+                # case sensitivity
+                if (status_type.lower() == "duplicate"):
+                    status_type = "Duplicate"
+                    parent_radar_number = status_words[1].strip()
+                    if (parent_radar_number.isdigit()):
+                        self.parent_number = parent_radar_number
+                    # Put the components back together
+                    current_status = status_type + "/" + parent_radar_number
+            # Update self.status with the sanitized status
+            self.status = current_status;
+        
+        if (self.title):
+            self.title = self.title.strip()
+    
+    def toDictionary(self):
+        return {
+            "id":self.key().id(),
+            "title":self.title,
+            "number":self.number,
+            "user":self.user.email(),
+            "status":self.status,
+            "description":self.description,
+            "resolved":self.resolved,
+            "product":self.product,
+            "classification":self.classification,
+            "reproducible":self.reproducible,
+            "product_version":self.product_version,
+            "originated":self.originated}
+    
 
-  created = db.DateTimeProperty()	# when the OpenRadar object was created
-  # These remaining properties are managed by the OpenRadar web app.
-  # They are automatically set when put() is called.
-  # We will add more as needed to allow better performance or to simplify sorting and querying.
-  modified = db.DateTimeProperty()	# when the OpenRadar object was last modified
-
-  def username(self):
-    return self.user.nickname().split("@")[0]
-
-  def put(self):
-    self.modified = datetime.datetime.now()
-    # Sanitize the data before storing
-    self.sanitize()
-    db.Model.put(self)
-    
-  def comments(self):
-    return Comment.gql("WHERE radar = :1 AND is_reply_to = :2", self, None)
-    
-  def children(self):
-    return Radar.gql("WHERE parent_number = :1 ORDER BY number ASC", self.number)
-    
-  def parent(self):
-    return Radar.gql("WHERE number = :1", self.parent_number).get()
-    
-  def sanitize(self):
-      self.classification = self.classification.strip()
-      self.description = self.description.strip()
-      self.number = self.number.strip()
-      self.originated = self.originated.strip()
-      self.product = self.product.strip()
-      self.product_version = self.product_version.strip()
-      self.resolved = self.resolved.strip()
-      self.reproducible = self.reproducible.strip()
-      
-      # The most common format for duplicates is "Duplicate/<radar_number>"
-      # If that format is found, extract the included radar number and store 
-      # it in self.parent_number
-      current_status = self.status.strip()
-      status_words = current_status.split("/")
-      if len(status_words) == 2:
-          # Trim any leading or trailing whitespace from the status type
-          status_type = status_words[0].strip()
-          # Determine whether status_type equals "duplicate", ignore case
-          # sensitivity
-          if status_type.lower() == "duplicate":
-              status_type = "Duplicate"
-              parent_radar_number = status_words[1].strip()
-              if parent_radar_number.isdigit():
-                  self.parent_number = parent_radar_number
-              # Put the components back together
-              current_status = status_type + "/" + parent_radar_number
-      # Update self.status with the sanitized status
-      self.status = current_status;
-      
-      self.title = self.title.strip()
-    
-  def toDictionary(self):
-    return {
-      "id":self.key().id(),
-      "title":self.title, 
-      "number":self.number,
-      "user":self.user.email(),
-      "status":self.status,
-      "description":self.description,
-      "resolved":self.resolved,
-      "product":self.product,
-      "classification":self.classification,
-      "reproducible":self.reproducible,
-      "product_version":self.product_version,
-      "originated":self.originated}
-
-import markdown
 md = markdown.Markdown()
 
 class Comment(search.SearchableModel):
